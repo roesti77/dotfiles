379f3d956f8294d558a4ae5de6fbaa9dc4c35d71
Robert Schneider <robert@vtrs.biz>
2025-01-02 00:49:38 +0100

    feat: add zelliy, nvim: ufo, zenmode, noice

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -4,11 +4,7 @@ return {
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.erb_lint,
-        null_ls.builtins.diagnostics.rubocop,
-        null_ls.builtins.formatting.rubocop,
+        null_ls.builtins.formatting.stylua
       },
     })
 
