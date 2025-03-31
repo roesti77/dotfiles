e7f7996923c3b35b0efdda909abaeb0af89e852d
Robert Schneider <roesti77@users.noreply.github.com>
2025-03-31 12:59:44 +0200

    fix: changed ollama model for avante

--- nvim/.config/nvim/lua/plugins/ai/avante.lua
+++ nvim/.config/nvim/lua/plugins/ai/avante.lua
@@ -1,44 +1,39 @@
 return {
-  "yetone/avante.nvim",
-  event = "VeryLazy",
+  'yetone/avante.nvim',
+  event = 'VeryLazy',
   lazy = false,
   version = false, -- set this if you want to always pull the latest change
   opts = {
     -- Add the Ollama provider configuration here
-    provider = "ollama", -- Use the Ollama provider
-    vendors = {
-      ollama = {
-        __inherited_from = "openai", -- Inherit general OpenAI settings
-        api_key_name = "", -- No API key needed for local Ollama server
-        endpoint = "http://127.0.0.1:11434/v1", -- Local Ollama endpoint
-        model = "llama3.2:1b", -- Use the desired model: qwen2.5-coder:14b
-      },
+    provider = 'ollama', -- Use the Ollama provider
+    ollama = {
+      model = 'qwq:32b',
     },
     behaviour = {
       auto_suggestions = false, -- Optional: disable auto suggestions
     },
     mappings = {
       suggestion = {
-        accept = "<M-l>",
-        next = "<M-]>",
-        prev = "<M-[>",
-        dismiss = "<C-]>",
+        accept = '<M-l>',
+        next = '<M-]>',
+        prev = '<M-[>',
+        dismiss = '<C-]>',
       },
     },
   },
-  build = "make", -- build command if you need it
+  build = 'make', -- build command if you need it
   dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "stevearc/dressing.nvim",
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
+    'nvim-treesitter/nvim-treesitter',
+    'stevearc/dressing.nvim',
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
     -- The below dependencies are optional
-    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    "zbirenbaum/copilot.lua", -- for providers='copilot'
+    'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+    'zbirenbaum/copilot.lua', -- for providers='copilot'
     {
       -- support for image pasting
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
+      'HakonHarnes/img-clip.nvim',
+      event = 'VeryLazy',
       opts = {
         default = {
           embed_image_as_base64 = false,
