6b1465c388d5aa0459fe469278317ada87403598
Robert Schneider <roesti77@users.noreply.github.com>
2025-06-12 08:27:10 +0200

    feat: codeium keybindings

--- nvim/.config/nvim/lua/plugins/ai/codeium.lua
+++ nvim/.config/nvim/lua/plugins/ai/codeium.lua
@@ -1,4 +1,15 @@
 return {
-  'Exafunction/codeium.vim',
-  event = 'BufEnter'
+    'Exafunction/codeium.vim',
+    event = 'BufEnter',
+    config = function()
+        vim.keymap.set('i', '<C-f>', function()
+            return vim.fn['codeium#CycleCompletions'](1)
+        end, { expr = true })
+        vim.keymap.set('i', '<C-b>', function()
+            return vim.fn['codeium#CycleCompletions'](-1)
+        end, { expr = true })
+        vim.keymap.set('i', '<C-]>', function()
+            return vim.fn['codeium#Clear']()
+        end, { expr = true })
+    end,
 }
